d512fad317690c5aaa37c3ea0a7d6d1330db7dcc
alirezanava72 <alirezanava72@gmail.com>
2023-09-05 15:54:15 +1000

    noteSchema

---
+++ models/note.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const { Timestamp } = require('mongodb');
+
+const SALT_ROUNDS = 6;
+
+const noteSchema = new Schema({
+    text: {type: String, required: true},
+    user: {type: mongoose.Schema.Types.ObjectId, required: true}
+  }, 
+  
+  {
+    timestamps: true,
+   
+  });
+
+  module.exports = mongoose.model('Note', noteSchema);
